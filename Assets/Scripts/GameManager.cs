64a70b021a9bbb9b3a5b6fccc63ddf36246ceed9
shadel96 <shadel96@gmail.com>
2021-09-13 21:27:20 -0400

    Singleton Manager
    Manager is singleton, loader script loads manager, etc.
    
    pat on back.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,12 +4,27 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance = null;
+
+
     public BoardManager boardScript;
 
-    private int level = 3;
+    private int level = 4;
 
     void Awake()
     {
+        //hot singletons in your area am i right ladies
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else if( instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+
         boardScript = GetComponent<BoardManager>();
         InitGame();
     }
